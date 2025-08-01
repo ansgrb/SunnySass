843930c4c9c4d5da6167af7ab592f1d2d458dab8
ansgrb <ansgrb@gmail.com>
2025-08-01 18:20:10 +0300

    :tada: Initialize project with Spring Boot, Kotlin, and Gradle configuration.

---
+++ SunnySass/src/main/kotlin/com/github/ansgrb/sunnysass/SunnySassApplication.kt
@@ -0,0 +1,11 @@
+package com.github.ansgrb.sunnysass
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SunnySassApplication
+
+fun main(args: Array<String>) {
+	runApplication<SunnySassApplication>(*args)
+}
---
+++ SunnySass/src/test/kotlin/com/github/ansgrb/sunnysass/SunnySassApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.ansgrb.sunnysass
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SunnySassApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
